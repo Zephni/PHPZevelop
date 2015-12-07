f278d3b346203ac2ffc93e688651dd1ff1050df0
Craig Dennis <contact@zephni.com>
2015-12-07 12:00:55 +0000

    Discus comments added

--- site/pages/home.php
+++ site/pages/home.php
@@ -6,16 +6,27 @@
 	));
 ?>
 
-
 <h2>Home</h2>
 <p><a href="https://github.com/Zephni/PHPZevelop">PHPZevelop</a> is a PHP framework for ease of use and adaptability. I am building it for personal use -  
-but if anybody wishes to fork it please feel free! Any comments and ideas are welcome, you can email me on: contact at zephni.com</p>
+but if anybody wishes to fork it please feel free! Any comments and ideas are welcome, you can also email me on: contact at zephni.com</p>
 
-<b>My ethos is to create a platform that provides a standard basis for website structure, but readily organised and quick to develop upon.
-It should always use standard PHP and HTML without convoluting and over-complicating things.</b>
+<div id="disqus_thread"></div>
+<script>
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
 
-<p>Downloading the framework and running on a PHP webserver (Whether windows or unix) by default will display this help site for explaining how to use it.
-As this is being written PHPZevelop is in very early stages and the help files are still being compiled.</p>
+s.src = '//phpzevelop.disqus.com/embed.js';
+
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
+<b><br />My ethos is to create a platform that provides an intuitive website structure that is well organised and quick to develop upon.
+It should always use standard PHP and HTML and be fully adaptable without fuss.</b>
+
+<p>Downloading the framework and running on a PHP webserver (Whether windows or unix) by default will display this help site for explaining how to use it.</p>
 
 <p>The file structure for PHPZevelop is as follows: </p>
 
