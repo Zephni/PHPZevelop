03677c7a85433eff9a4e83c307688f680e9d42fa
Craig Dennis <contact@zephni.com>
2015-12-08 07:18:40 +0000

    site/instantiate.php checks for class definitions before instantiating

--- site/instantiate.php
+++ site/instantiate.php
@@ -1,13 +1,20 @@
 <?php
 	/* Database
-	------------------------------*/	
-	if(strlen($PHPZevelop->CFG->DB->Host) > 0){
-		$DB = new db($PHPZevelop->CFG->DB->Host, $PHPZevelop->CFG->DB->User, $PHPZevelop->CFG->DB->Pass, $PHPZevelop->CFG->DB->Name);
-		
-		if(!$DB->Connected)
-			die($DB->ErrorHandler());
+	------------------------------*/
+	if(class_exists("DB"))
+	{
+		if(strlen($PHPZevelop->CFG->DB->Host) > 0)
+		{
+			$DB = new DB($PHPZevelop->CFG->DB->Host, $PHPZevelop->CFG->DB->User, $PHPZevelop->CFG->DB->Pass, $PHPZevelop->CFG->DB->Name);
+			
+			if(!$DB->Connected)
+				die($DB->ErrorHandler());
+		}
 	}
 
-	/* Instantiate
+	/* Link
 	------------------------------*/
-	$Link = new Link($PHPZevelop->Path);
+	if(class_exists("Link"))
+	{
+		$Link = new Link($PHPZevelop->Path);
+	}
