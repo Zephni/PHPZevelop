3a2ad58f7d94601ef86339262785234c4967d403
Craig Dennis <contact@zephni.com>
2015-12-11 11:17:13 +0000

    Nav change non mobile

--- howto/templates/main.php
+++ howto/templates/main.php
@@ -54,19 +54,17 @@
 					);
 				?>
 
-				<table id="nav" class="non-mobile-only">
+				<nav id='nav' class="non-mobile-only"><ul>
 					<?php
 						foreach($NavItems as $Name => $Array)
 						{
-							echo "<tr><td class='head'>".$Name."</td></tr>";
-							echo "<tr><td><table style='width: 100%;'>";
-							foreach($Array as $Value){
-								echo "<td>".$Value."</td>";
-							}
-							echo "</table></td></tr>";
+							echo "<li><span class='head'>".$Name."</span><ul>";
+							foreach($Array as $Value)
+								echo "<li>".$Value."</li>";
+							echo "</ul></li>";
 						}
 					?>
-				</table>
+				</ul><div style="width: 100%; clear: both;"></div></nav>
 
 				<div id="nav" class="mobile-only">
 					<?php
