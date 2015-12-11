167feb6398d4c2c1bb7e9379771e583c8d72e873
Craig Dennis <contact@zephni.com>
2015-12-11 05:20:12 +0000

    Trimmed up some lines

--- phpzevelop/initiate.php
+++ phpzevelop/initiate.php
@@ -93,15 +93,12 @@
 	------------------------------*/
 	$PHPZevelop->NewObject("Path", new Path($PHPZevelop->CFG));
 
-	/* Check if parameters have been passed
+	/* Check if parameters have been passed, and set them in $_GET
 	------------------------------*/
 	if(count($PHPZevelop->Get("URLParameters")) > 0)
 	{
-		// Convert to $_GET
-		$Params = array_reverse($PHPZevelop->Get("URLParameters"));
-		for($i = 0; $i < count($Params); $i++)
-			$_GET[$PHPZevelop->CFG->PreParam.$i] = $Params[$i];
-		unset($Params);
+		foreach(array_reverse($PHPZevelop->Get("URLParameters")) as $key => $value)
+			$_GET[$PHPZevelop->CFG->PreParam.$key] = $value;
 	}
 
 	/* Site specific
