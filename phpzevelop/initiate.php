37be4c685f189d12baba1c848b71286a93938a80
Craig Dennis <contact@zephni.com>
2015-12-14 02:39:26 +0000

    Trimmed up some lines

--- phpzevelop/initiate.php
+++ phpzevelop/initiate.php
@@ -97,11 +97,8 @@
 	------------------------------*/
 	if(count($PHPZevelop->Get("URLParameters")) > 0)
 	{
-		// Convert to $_GET
-		$Params = array_reverse($PHPZevelop->Get("URLParameters"));
-		for($i = 0; $i < count($Params); $i++)
-			$_GET[$PHPZevelop->CFG->PreParam.$i] = $Params[$i];
-		unset($Params);
+		foreach(array_reverse($PHPZevelop->Get("URLParameters")) as $k => $v)
+			$_GET[$PHPZevelop->CFG->PreParam.$k] = $v;
 	}
 
 	/* Site specific
