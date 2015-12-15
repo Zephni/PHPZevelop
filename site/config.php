66ff4a6ac859a413f3124cc8264dbf5be9e40d09
Craig Dennis <contact@zephni.com>
2015-12-15 09:09:33 +0000

    v1.67

--- site/config.php
+++ site/config.php
@@ -1,10 +1,6 @@
 <?php
 	/* Config
-	------------------------------*/
-	$PHPZevelop->OverrideObjectData("CFG", array(
-		"SiteTitle" 	=> "New project"
-	));
-	
+	------------------------------*/	
 	$PHPZevelop->CFG->DB = (object) array(
 		"Host" => "",
 		"User" => "",
