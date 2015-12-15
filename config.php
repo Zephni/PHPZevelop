1cc7c092b695778135e6b2f5dad97a16f32da74f
Craig Dennis <contact@zephni.com>
2015-12-15 04:59:33 +0000

    v1.65

--- config.php
+++ config.php
@@ -11,7 +11,7 @@
 
 		// Structural
 		"Site"				=> "site",
-		"MultiSite"			=> array("howto", "admin"),
+		"MultiSite"			=> array("howto"),
 		"LoadStyle"			=> "Template",
 		"Template"			=> "main",
 		"PassParams"		=> false,
