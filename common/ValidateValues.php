f8fdca1745afe1330fccf473d8493f1bfae7312e
Craig Dennis <contact@zephni.com>
2018-01-30 10:40:52 +0000

    Updated ValidateValues static class to be more streamlined for usage

--- common/ValidateValues.php
+++ common/ValidateValues.php
@@ -5,6 +5,15 @@
 		public static $ValidPairs = array();
 		public static $InvalidPairs = array();
 
+		/*
+			Use like:
+			ValidateValues::Run($_POST, array(
+				"username" => function($Value){
+					if(strlen($Value) < 6)
+						return "Name must be at least 6 characters";
+				}
+			));
+		*/
 		public static function Run($Data, $CheckMethods = array(), $_Options = array())
 		{
 			$Options = array_merge(array(
@@ -13,14 +22,20 @@
 
 			foreach($Data as $Key => $Value)
 			{
-				$OutputString = "";
-				$Valid = (isset($CheckMethods[$Key])) ? $CheckMethods[$Key]($Value, $OutputString) : true;
-				if($Valid === null) $Valid = true;
+				$Output = (isset($CheckMethods[$Key])) ? $CheckMethods[$Key]($Value) : true;
+				
+				if($Output === null)
+					$Output = true;
 
-				if(!$Valid) self::$ErrorMessages[] = "<span style='color: ".$Options["ErrorColor"].";'>".$OutputString."</span>";
-
-				if($Valid) self::$ValidPairs[$Key] = $Data[$Key];
-				else self::$InvalidPairs[$Key] = $Data[$Key];
+				if($Output !== true)
+				{
+					self::$ErrorMessages[] = "<span style='color: ".$Options["ErrorColor"].";'>".$Output."</span>";
+					self::$InvalidPairs[$Key] = $Data[$Key];
+				}
+				else
+				{
+					self::$ValidPairs[$Key] = $Data[$Key];
+				}
 			}
 		}
 	}
