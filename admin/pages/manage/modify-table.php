3e1f771719b1f2cc453a6abba51d9da274fc8b6d
Craig Dennis <contact@zephni.com>
2018-10-02 11:31:47 +0100

    Modify table fields removes new lines and single quotes

--- admin/pages/manage/modify-table.php
+++ admin/pages/manage/modify-table.php
@@ -74,7 +74,7 @@
 	$(document).ready(function(){
 		FieldHTML =  "<tr class='field'><td>Field <span class='removeField'>(remove)</span></td></tr><tr><td>";
 		FieldHTML += "<input type='text' name='field_name[]' style='width: 24%; margin-right: 1.3%;' placeholder='field name' />";
-		FieldHTML += "<select type='text' name='field_type[]' style='width: 24%; margin-right: 1.3%;'><option>VARCHAR(255)</option><option>TEXT</option></select>";
+		FieldHTML += "<select type='text' name='field_type[]' style='width: 24%; margin-right: 1.3%;'><option>TEXT</option></select>";
 		FieldHTML += "<input type='text' name='field_default[]' style='width: 24%; margin-right: 1.2%;' placeholder='field default' />";
 		FieldHTML += "<input type='text' name='field_comment[]' style='width: 24%;' placeholder='field comment' />";
 		FieldHTML += "</td></tr>";
@@ -86,16 +86,16 @@
 					continue;
 				?>
 				LoadedFieldHTML =  "<tr class='field'><td>Field <span class='removeField'>(remove)</span></td></tr><tr><td>";
-				LoadedFieldHTML += "<input type='hidden' name='original_field_name[]' style='width: 24%; margin-right: 1.3%;' value='<?php echo $K; ?>' />";
-				LoadedFieldHTML += "<input type='text' name='field_name[]' style='width: 24%; margin-right: 1.3%;' placeholder='field name' value='<?php echo $K; ?>' />";
+				LoadedFieldHTML += "<input type='hidden' name='original_field_name[]' style='width: 24%; margin-right: 1.3%;' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $K)); ?>' />";
+				LoadedFieldHTML += "<input type='text' name='field_name[]' style='width: 24%; margin-right: 1.3%;' placeholder='field name' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $K)); ?>' />";
 
-				LoadedFieldHTML += "<select type='text' name='field_type[]' style='width: 24%; margin-right: 1.3%;'><option>VARCHAR(255)</option><option>TEXT</option></select>";
+				LoadedFieldHTML += "<select type='text' name='field_type[]' style='width: 24%; margin-right: 1.3%;'><option>TEXT</option></select>";
 
-				LoadedFieldHTML += "<input type='hidden' name='original_field_default[]' style='width: 24%; margin-right: 1.3%;' value='<?php echo $V["column_default"]; ?>' />";
-				LoadedFieldHTML += "<input type='text' name='field_default[]' style='width: 24%; margin-right: 1.2%;' placeholder='field default' value='<?php echo $V["column_default"]; ?>' />";
+				LoadedFieldHTML += "<input type='hidden' name='original_field_default[]' style='width: 24%; margin-right: 1.3%;' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $V["column_default"])); ?>' />";
+				LoadedFieldHTML += "<input type='text' name='field_default[]' style='width: 24%; margin-right: 1.2%;' placeholder='field default' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $V["column_default"])); ?>' />";
 
-				LoadedFieldHTML += "<input type='hidden' name='original_field_comment[]' style='width: 24%;' value='<?php echo $V["column_comment"]; ?>' />";
-				LoadedFieldHTML += "<input type='text' name='field_comment[]' style='width: 24%;' placeholder='field comment' value='<?php echo $V["column_comment"]; ?>' />";
+				LoadedFieldHTML += "<input type='hidden' name='original_field_comment[]' style='width: 24%;' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $V["column_comment"])); ?>' />";
+				LoadedFieldHTML += "<input type='text' name='field_comment[]' style='width: 24%;' placeholder='field comment' value='<?php echo preg_replace("/\r?\n|\r/", "", str_replace("'", "", $V["column_comment"])); ?>' />";
 				LoadedFieldHTML += "</td></tr>";
 				$("#addField").parent().parent().before(LoadedFieldHTML);
 
