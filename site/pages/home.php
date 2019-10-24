b1fcdf815fc50cb58b4f690a7e02fd5e8b7afa0a
Craig Dennis <zephni@hotmail.co.uk>
2019-10-24 23:41:53 +0100

    Removed search from home

--- site/pages/home.php
+++ site/pages/home.php
@@ -64,57 +64,51 @@
 	<div id="sideBar" class="col-xs-3 hidden-xs">
 		<div class="row">
 			<div id="sidebarNav">
-				<form class="form">
-					<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
-					</div>
-				</form>
-
 				<nav>
-				<ul class="nav nav-stacked">
-					<li><a href="#Home">Home</a></li>
-					<?php
-						ScanDirectory($PHPZevelop->CFG->Site."/howto", function($Item){
-							global $PHPZevelop;
-							if(substr($Item, 0, 1) != ".")
-							{
-								echo "<li class='nav-item'>";
-								echo "<a class='sideNavItem' href='#".PrettyTitle($Item, true)."'>".PrettyTitle($Item)."</a>";
-								
-								ScanDirectory($PHPZevelop->CFG->Site."/howto/".$Item, function($Item2){
-									echo "<li class='nav-item' style='padding-left: 15px;'><a class='sideNavItem' href='#".PrettyTitle($Item2, true)."'>".PrettyTitle($Item2)."</a></li>";
-								});
-	
-								echo "</li>";
-							}
-						});
-
-						function ScanDirectory($Path, $Callback)
-						{
-							foreach(scandir($Path) as $Item)
-							{
+					<ul class="nav nav-stacked">
+						<li><a href="#Home">Home</a></li>
+						<?php
+							ScanDirectory($PHPZevelop->CFG->Site."/howto", function($Item){
+								global $PHPZevelop;
 								if(substr($Item, 0, 1) != ".")
 								{
-									$CurrentDir = explode("/", $Path);
-									$CurrentDir = $CurrentDir[count($CurrentDir)-1];
-									$Callback($Item, $CurrentDir);
+									echo "<li class='nav-item'>";
+									echo "<a class='sideNavItem' href='#".PrettyTitle($Item, true)."'>".PrettyTitle($Item)."</a>";
+									
+									ScanDirectory($PHPZevelop->CFG->Site."/howto/".$Item, function($Item2){
+										echo "<li class='nav-item' style='padding-left: 15px;'><a class='sideNavItem' href='#".PrettyTitle($Item2, true)."'>".PrettyTitle($Item2)."</a></li>";
+									});
+		
+									echo "</li>";
+								}
+							});
+
+							function ScanDirectory($Path, $Callback)
+							{
+								foreach(scandir($Path) as $Item)
+								{
+									if(substr($Item, 0, 1) != ".")
+									{
+										$CurrentDir = explode("/", $Path);
+										$CurrentDir = $CurrentDir[count($CurrentDir)-1];
+										$Callback($Item, $CurrentDir);
+									}
 								}
 							}
-						}
 
-						function PrettyTitle($Title, $RemoveSpaces = false)
-						{
-							$Title = str_replace("-", " ", $Title);
-							$Title = str_replace(".php", "", $Title);
-							$Title = ucfirst($Title);
-							if($RemoveSpaces){
-								$Title = ucwords($Title);
-								$Title = str_replace(" ", "", $Title);
+							function PrettyTitle($Title, $RemoveSpaces = false)
+							{
+								$Title = str_replace("-", " ", $Title);
+								$Title = str_replace(".php", "", $Title);
+								$Title = ucfirst($Title);
+								if($RemoveSpaces){
+									$Title = ucwords($Title);
+									$Title = str_replace(" ", "", $Title);
+								}
+								return $Title;
 							}
-							return $Title;
-						}
-					?>
-				</ul>
+						?>
+					</ul>
 				</nav>
 			</div>
 		</div>
@@ -122,7 +116,7 @@
 </div>
 
 <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
+<div class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
@@ -143,7 +137,6 @@
 			<li>Logical page and file structure</li>
 			<li>Automatically makes URLs pretty (ie. website.com/about rather than website.com/about.php)</li>
 			<li>Multiple load styles based on preference</li>
-			<li>Default template comes with Bootstrap</li>
 			<li>Multiple sites on a single PHPZevelop installation</li>
 		</ul>
       </div>
